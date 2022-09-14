e65afab37765190edae571f9bd68c943a24f89cd
Kenneth Hedman <github@vitberget.se>
2022-09-14 21:49:32 +0200

    changes

--- src/se/vitberget/val/val.clj
+++ src/se/vitberget/val/val.clj
@@ -5,11 +5,13 @@
 (def url "https://resultat.val.se/assets/valdata/RD_00_P_mandat.json")
 (def filen "/home/k/src/val-analys/val.json")
 
-(defn dirty-string->number [string]
-  (->> string
-       (filter (fn [c] (Character/isDigit c)))
-       (apply str)
-       (read-string)))
+(defn dirty-string->number [input]
+  (if (number? input)
+    input
+    (->> input
+         (filter (fn [c] (Character/isDigit c)))
+         (apply str)
+         (read-string))))
 
 (defn procent
   [namnare taljare]
@@ -18,47 +20,29 @@
        (format "%.2f%%")))
 
 (defn valet []
-  (let [v (json/read (io/reader filen)
-                     :key-fn keyword)
-        antalRostberattigade (->> v
-                                  (:antalRostberattigade)
+  (let [val-data (json/read (io/reader filen) :key-fn keyword)
+        antalRostberattigade (->> val-data
+                                  (:antalRostberattigadeIRaknadeValdistrikt)
                                   (dirty-string->number))
-        rosterPaverkaMandat (->> v
+        soffliggare (- antalRostberattigade
+                       (->> val-data
+                            (:totaltAntalRoster)
+                            (dirty-string->number)))
+        items (as-> (concat (->> val-data
                                  (:rosterPaverkaMandat)
-                                 (:antalRoster)
-                                 (dirty-string->number))
-
-        rosterEjPaverkaMandat (->> v
-                                   (:rosterEjPaverkaMandat)
-                                   (:antalRoster)
-                                   (dirty-string->number))
-
-        soffliggare (- antalRostberattigade rosterPaverkaMandat rosterEjPaverkaMandat)
-
-
-
-        partier (->> v
-                     (:rosterPaverkaMandat)
-                     (:partiroster))
-
-        ogiltiga (->> v
-                      (:rosterEjPaverkaMandat)
-                      (:partiroster))
-
-        items (-> (concat partier ogiltiga)
-                  (conj {:antalRoster      (str soffliggare)
-                         :partibeteckning  "Soffliggare"
-                         :partiforkortning "X"}))
-        items (->> items
-                   (map (fn [b] (-> b
-                                    (select-keys [:antalRoster :partibeteckning :partiforkortning])
-                                    (update :antalRoster dirty-string->number))))
-                   (map (fn [b] (assoc b :procent (procent (:antalRoster b) antalRostberattigade))))
-                   (sort (fn [a b] (> (:antalRoster a) (:antalRoster b)))))
-
-        ]
+                                 (:partiroster))
+                            (->> val-data
+                                 (:rosterEjPaverkaMandat)
+                                 (:partiroster))) $
+                    (conj $ {:antalRoster      soffliggare
+                             :partibeteckning  "Soffliggare"
+                             :partiforkortning "X"})
+                    (map (fn [b] (-> b
+                                     (select-keys [:antalRoster :partibeteckning :partiforkortning])
+                                     (update :antalRoster dirty-string->number))) $)
+                    (map (fn [b] (assoc b :procent (procent (:antalRoster b) antalRostberattigade))) $)
+                    (sort (fn [a b] (> (:antalRoster a) (:antalRoster b))) $))]
     (for [p items]
-      (println (:procent p) (:partibeteckning p)))
-    ))
+      (println (:procent p) (:partibeteckning p)))))
 
 (valet)
